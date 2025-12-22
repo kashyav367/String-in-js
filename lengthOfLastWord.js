8ee0f47447712b157c99608fd6e4cc8ca13a1df7
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-12-22 15:31:46 +0530

    2nd

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -1,9 +1,64 @@
-function lengthOfLastWord(s){
-     s = s.trim();
-    s = s.split(" ");
+// function lengthOfLastWord(s){
+//      s = s.trim();
+//     s = s.split(" ");
 
-    console.log(s[s.length-1].length);
+//     console.log(s[s.length-1].length);
+// } 
+
+// let s = "Hello World"
+// lengthOfLastWord(s);
+
+// function lengthOfLastWord(s){
+//     let n = s.length-1
+
+//     while(n >= 0){
+//         if(s[n] === " "){
+//             --n;
+//         }
+//         else{
+//             break;
+//         }
+//     }
+
+//     let count = 0;
+
+//     while(n >= 0){
+//         if(s[n] != " "){
+//             --n;
+//             count++;
+//         }
+//         else{
+//             break;
+//         }
+//     }
+//     return count;
+// } 
+
+// let s = "Hello World"
+
+// let result = lengthOfLastWord(s);
+// console.log(result);
+
+
+function lengthOfLastWord(s){
+    let n = s.length-1
+
+    while(n >= 0){
+        if(s[n] != " ") break;
+         --n;
+    }
+
+    let count = 0;
+
+    while(n >= 0){
+        if(s[n] === " ") break;
+            --n;
+            count++;
+    }
+    return count;
 } 
 
 let s = "Hello World"
-lengthOfLastWord(s);
+
+let result = lengthOfLastWord(s);
+console.log(result);
