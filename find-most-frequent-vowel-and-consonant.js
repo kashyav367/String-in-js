eed2689e4d94aa72453c74ce8b81c3df41611cff
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-12-25 18:07:20 +0530

    6th commit

---
+++ find-most-frequent-vowel-and-consonant.js
@@ -0,0 +1,34 @@
+function maxFreqSum(s){
+
+    let map = {};
+
+    for(let i=0 ; i<s.length ; i++){
+        if(!map[s[i]]){
+            map[s[i]] = 1;
+        }
+        else{
+            ++map[s[i]];
+        }
+    }
+
+    let vowels = ['a' , 'e' , 'i' , 'o' ,'u'];
+    let maxVowel = 0;
+    let maxConsonant = 0;
+    
+    for(let i=0 ; i<s.length ; i++){
+        if(vowels.includes(s[i])){
+            if(map[s[i]] > maxVowel){
+                maxVowel = map[s[i]];
+            }
+        } 
+            else{
+                if(map[s[i]] > maxConsonant){
+                    maxConsonant = map[s[i]];
+                }
+            }
+        }
+    return maxConsonant + maxVowel;
+}
+
+ let s = "aeiaeia";
+ console.log(maxFreqSum(s));
