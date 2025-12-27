9c8db7be12c001ba030ad8ba1cb361d7c40ba3ee
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-12-27 19:37:03 +0530

    9th commit

--- split-a-string-in-balanced-strings.js
+++ split-a-string-in-balanced-strings.js
@@ -1,22 +1,46 @@
 
+// //brute force
+// function balancedStringSplit(s){
+//     let L = 0;
+//     let R = 0;
+//     let count = 0;
+
+//     for(let i=0 ; i<s.length ; i++){
+//         if(s[i] === "R"){
+//             R++;
+//         }
+//         else{
+//             ++L;
+//         }
+
+//         if(L === R){
+//             count++;
+//             L=0;
+//             R=0;
+//         }
+//     }
+//     return count;
+// }
+
+// let  s = "RLRRLLRLRL";
+// console.log(balancedStringSplit(s));
+
+//optimised code
 //brute force
 function balancedStringSplit(s){
-    let L = 0;
-    let R = 0;
+    let temp = 0;
     let count = 0;
 
     for(let i=0 ; i<s.length ; i++){
         if(s[i] === "R"){
-            R++;
+            ++temp;
         }
         else{
-            ++L;
+            --temp;
         }
 
-        if(L === R){
+        if(temp === 0){
             count++;
-            L=0;
-            R=0;
         }
     }
     return count;
@@ -24,3 +48,4 @@ function balancedStringSplit(s){
 
 let  s = "RLRRLLRLRL";
 console.log(balancedStringSplit(s));
+
