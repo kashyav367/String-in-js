b3f98dc9c41c3dfe6c27021a714c7d10f2ff8995
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-12-29 20:27:54 +0530

    13th  commit

---
+++ Valid-palindrome-approach-extra-space.js
@@ -0,0 +1,15 @@
+function validPalindrome(s){
+    s = s.toLowerCase();
+
+    let filteredString = "";
+    for(let i=0 ; i<s.length ; i++){
+        if(s[i].match(/[a-z0-9]/i)){
+            filteredString += s[i];
+        }
+    }
+    let rev = filteredString.split("").reverse().join("");
+    return filteredString === rev;
+}
+
+let s = "A man, a plan, a canal: Panama"
+console.log(validPalindrome(s));
