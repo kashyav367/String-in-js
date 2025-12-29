a415c021510bf216789ae736e54c140681988cad
Ankit kumar Singh <ankit12030621028@gmail.com>
2025-12-29 12:22:47 +0530

    12th commit

---
+++ ReverseString-II.js
@@ -0,0 +1,18 @@
+function reverseString(s,k){
+    s = s.split("");
+
+    for(let x = 0; x < s.length ; x = x+(2*k)){
+        let n = k;
+        let mid = Math.floor(k/2);
+
+        for(let i=0 ; i<mid ; i++){
+            let temp = s[x+i];
+            s[x+i] = s[x+n-1-i];
+            s[x+n-1-i] = temp;
+        }
+    }
+    return s.join("");
+}
+
+let  s = "abcdefg", k = 2;
+console.log(reverseString(s,k));
