831fa978383213d91c534ed4b1825e260e7fd0a9
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-01-04 21:55:11 +0530

    19th commit

---
+++ group-anagram-sorting-approach.js
@@ -0,0 +1,19 @@
+// function groupAnagram(strs){
+
+//     let map = {};
+
+//     for(let i=0 ; i<strs.length ; i++){
+//         let sortedStr = strs[i].split("").sort().join("");
+
+//         if(!map[sortedStr]){
+//             map[sortedStr] = [strs[i]];
+//         }
+//         else{
+//             map[sortedStr].push(strs[i]);
+//         }
+//     }
+//     return [...Object.values(map)];
+// }
+
+// let strs = ["eat","tea","tan","ate","nat","bat"];
+// console.log(groupAnagram(strs));
